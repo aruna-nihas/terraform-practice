06222e1ddc695301647bddad0319c39ffd5b5fc7
aruna <arunadegala18@gmail.com>
2024-06-28 12:05:41 +0530

    terraform

---
+++ variables/ec2-sg-variables.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "ec2" {
+    ami = var.ami_id
+    instance_type = "t2.micro"
+    tags = {
+        Name = "Ec2"
+    }
+  
+}
+resource "aws_security_group""sg"{
+    name = "allow-all-ports"
+    tags = {
+        Name = "Internet"
+    }
+    ingress {
+        from_port = "0"
+        to_port = "0"
+        protocol = "tcp"
+        cidr_blocks = var.cidr_blocks
+    }
+    egress {
+        from_port = "0"
+        to_port = "0"
+        protocol = "-1"
+        cidr_blocks = var.cidr_blocks
+    }
+}
---
+++ variables/variables.tf
@@ -0,0 +1,9 @@
+variable "ami_id" {
+    type = string
+    default = "ami-0b4f379183e5706b9"
+  
+}
+variable "cidr_blocks" {
+    default = ["0.0.0.0/0"]
+  
+}
